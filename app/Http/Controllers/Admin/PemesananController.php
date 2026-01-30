e4c32be68ebbd5bb5b5b6d89e44c24c1b120cc5b
andra343 <setiawanananda32@gmail.com>
2026-01-30 07:58:14 +0700

    perbaikan bug

--- app/Http/Controllers/Admin/PemesananController.php
+++ app/Http/Controllers/Admin/PemesananController.php
@@ -13,32 +13,47 @@ class PemesananController extends Controller
 
     public function index()
     {
-        //
-    }
+        $items      = Pemesanan::with(['user', 'kamar', 'fasilitas'])->latest()->get();
+        $penyewa    = User::where('role', 'penyewa')->get();
+        $fasilitas  = Fasilitas::all();
+        // kirim waktu sekarang
+        $now = now();
 
-    public function create()
-    {
-        //
+        return view('pages.admins.pemesanan.data-pemesanan', compact('items', 'penyewa', 'fasilitas', 'now'));
     }
 
     public function store(Request $request)
     {
-        //
-    }
 
-    public function show(Pemesanan $pemesanan)
-    {
-        //
-    }
+        $kamar = Kamar::findOrFail($request->kamar_id);
+        $Masuk = Carbon::parse($request->tgl_masuk);
 
-    public function edit(Pemesanan $pemesanan)
-    {
-        //
-    }
+        if ($request->jenis_sewa == 'Bulanan') {
+            $Keluar = $Masuk->copy()->addMonths($request->durasi);
+        } else {
+            $Keluar = $Masuk->copy()->addDays($request->durasi);
+        }
 
-    public function update(Request $request, Pemesanan $pemesanan)
-    {
-        //
+        $fasilitas = Fasilitas::whereIn('id', $request->fasilitas_ids ?? [])->sum('harga');
+        $harga = ($kamar->harga + $fasilitas) * $request->durasi;
+
+        $pemesanan = Pemesanan::create([
+            'user_id'       => $request->user_id,
+            'kamar_id'      => $request->kamar_id,
+            'tgl_masuk'     => $Masuk,
+            'tgl_keluar'    => $Keluar,
+            'jenis_sewa'    => $request->jenis_sewa,
+            'total_harga'   => $harga,
+            'status'        => 'Aktif'
+        ]);
+
+        if($request->fasilitas_ids) {
+            foreach ($request->fasilitas_ids as $fId) {
+                $f = Fasilitas::find($fId);
+                $pemesanan->fasilitas()->attach($fId, ['harga_snap' => $f->harga]);
+            }
+        }
+
     }
 
     public function destroy(Pemesanan $pemesanan)
